5c4cdcf53aec59e07de55e82885d8509e507c6e6
ambuc <bucklnd2@illinois.edu>
2015-07-18 18:50:28 -0400

    better div stacking
    enabled inline-block for .containers - you’re gonna like how it looks

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ var renderObj = function (key, obj, parent, depth) {
   // Draw things in here
   // This should contain newParent
   var container = document.createElement('div');
+  
+  $(container).addClass('container');
 
   // Create an "each"able function for rendering something
   var renderer = function (func) {
@@ -35,7 +37,6 @@ var renderObj = function (key, obj, parent, depth) {
       func(e, obj[e], newParent, depth + 1);
     };
   };
-
 
   $(container).appendTo(parent);
 
@@ -61,7 +62,52 @@ var renderObj = function (key, obj, parent, depth) {
 };
 
 var sampleObj = {"foo":"bar","glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}, baz: "bat"};
+var obj2 = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "Product set",
+    "type": "array",
+    "items": {
+        "title": "Product",
+        "type": "object",
+        "properties": {
+            "id": {
+                "description": "The unique identifier for a product",
+                "type": "number"
+            },
+            "name": {
+                "type": "string"
+            },
+            "price": {
+                "type": "number",
+                "minimum": 0,
+                "exclusiveMinimum": true
+            },
+            "tags": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "minItems": 1,
+                "uniqueItems": true
+            },
+            "dimensions": {
+                "type": "object",
+                "properties": {
+                    "length": {"type": "number"},
+                    "width": {"type": "number"},
+                    "height": {"type": "number"}
+                },
+                "required": ["length", "width", "height"]
+            },
+            "warehouseLocation": {
+                "description": "Coordinates of the warehouse with the product",
+                "$ref": "http://json-schema.org/geo"
+            }
+        },
+        "required": ["id", "name", "price"]
+    }
+};
 
-renderObj('root', sampleObj, document.body);
+renderObj('root', obj2, document.body);
 
 // $( "div.panel" ).effect("scale", { percent: 50,origin:'center'}, 100);
