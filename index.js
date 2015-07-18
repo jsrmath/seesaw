fb1053fa6609c5d14709c0149d264b49ce20fa8e
ambuc <bucklnd2@illinois.edu>
2015-07-18 19:03:11 -0400

    added another test object

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var renderObj = function (key, obj, parent, depth) {
   // Draw things in here
   // This should contain newParent
   var container = document.createElement('div');
-  
+
   $(container).addClass('container');
 
   // Create an "each"able function for rendering something
@@ -107,7 +107,76 @@ var obj2 = {
         "required": ["id", "name", "price"]
     }
 };
-
-renderObj('root', obj2, document.body);
+var obj3 = [
+  {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "ppu": 0.55,
+    "batters":
+      {
+        "batter":
+          [
+            { "id": "1001", "type": "Regular" },
+            { "id": "1002", "type": "Chocolate" },
+            { "id": "1003", "type": "Blueberry" },
+            { "id": "1004", "type": "Devil's Food" }
+          ]
+      },
+    "topping":
+      [
+        { "id": "5001", "type": "None" },
+        { "id": "5002", "type": "Glazed" },
+        { "id": "5005", "type": "Sugar" },
+        { "id": "5007", "type": "Powdered Sugar" },
+        { "id": "5006", "type": "Chocolate with Sprinkles" },
+        { "id": "5003", "type": "Chocolate" },
+        { "id": "5004", "type": "Maple" }
+      ]
+  },
+  {
+    "id": "0002",
+    "type": "donut",
+    "name": "Raised",
+    "ppu": 0.55,
+    "batters":
+      {
+        "batter":
+          [
+            { "id": "1001", "type": "Regular" }
+          ]
+      },
+    "topping":
+      [
+        { "id": "5001", "type": "None" },
+        { "id": "5002", "type": "Glazed" },
+        { "id": "5005", "type": "Sugar" },
+        { "id": "5003", "type": "Chocolate" },
+        { "id": "5004", "type": "Maple" }
+      ]
+  },
+  {
+    "id": "0003",
+    "type": "donut",
+    "name": "Old Fashioned",
+    "ppu": 0.55,
+    "batters":
+      {
+        "batter":
+          [
+            { "id": "1001", "type": "Regular" },
+            { "id": "1002", "type": "Chocolate" }
+          ]
+      },
+    "topping":
+      [
+        { "id": "5001", "type": "None" },
+        { "id": "5002", "type": "Glazed" },
+        { "id": "5003", "type": "Chocolate" },
+        { "id": "5004", "type": "Maple" }
+      ]
+  }
+];
+renderObj('root', obj3, document.body);
 
 // $( "div.panel" ).effect("scale", { percent: 50,origin:'center'}, 100);
